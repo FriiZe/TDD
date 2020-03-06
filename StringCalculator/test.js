ae1984073851b87a9195bc30165e4185226d15b9
Romain <FriiZe@MacBook-Pro-de-FriiZe.local>
2020-03-06 12:49:21 +0100

    redphase SC

---
+++ StringCalculator/test.js
@@ -0,0 +1,16 @@
+const chai = require('chai'); // Using Expect style
+
+
+const { assert, expect } = chai;
+const model = require('./model')
+
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
+
+describe('Test TDD', () => {
+    describe('Test String calculator', async () => {
+        it('The method can take 0, 1 or 2 numbers separated by comma, and returns their sum.', () => {
+            assert.equal(model.string("1.1,2.2"),"3.3")
+        })
+    })  
+})
