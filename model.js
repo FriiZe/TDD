5b6b9b94eadb85fa89904ff629192607745b85c8
Romain <FriiZe@MacBook-Pro-de-FriiZe.local>
2020-03-06 11:14:43 +0100

    green fizz

--- model.js
+++ model.js
@@ -1,4 +1,10 @@
 module.exports = {
      fizz(num) {   
-    }
+         let ret = ""
+         if (Number.isInteger(num/3)){
+             ret += "Fizz"
+         }
+         
+         return ret
+     }
 }
