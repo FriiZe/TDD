a0dc030329e944cbd2ce7d5775933e9b3a65986c
Romain <FriiZe@MacBook-Pro-de-FriiZe.local>
2020-03-06 11:18:46 +0100

    refacto fizz

--- model.js
+++ model.js
@@ -1,10 +1,6 @@
 module.exports = {
      fizz(num) {   
          let ret = ""
-         if (Number.isInteger(num/3)){
-             ret += "Fizz"
-         }
-         
-         return ret
+         return Number.isInteger(num/3) ? ret+= "Fizz" : ret       
      }
 }
