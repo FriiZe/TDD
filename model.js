ccf6211c064ac639f574c57300326cf6f2b43773
Romain <FriiZe@MacBook-Pro-de-FriiZe.local>
2020-03-06 11:28:57 +0100

    refacto phase buzz

--- model.js
+++ model.js
@@ -2,9 +2,6 @@ module.exports = {
     fizzbuzz(num) {   
         let ret = ""
         Number.isInteger(num/3) ? ret+= "Fizz" : ret
-        if (Number.isInteger(num/5)){
-            ret+= "Buzz"   
-        }
-        return ret
+        return Number.isInteger(num/5) ? ret+= "Buzz" : ret        
     }
 }
