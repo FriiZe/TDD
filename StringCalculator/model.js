2bfb0db3f875cd3634e4496d74c99f124e27e15a
Romain <FriiZe@MacBook-Pro-de-FriiZe.local>
2020-03-06 16:30:55 +0100

    refacto custom separator

--- StringCalculator/model.js
+++ StringCalculator/model.js
@@ -1,17 +1,16 @@
 module.exports = {
     add(str) {
         if (str === "") return "0"
-        let result = 0 
         if (str.startsWith("//")){
             str = str.substring(2)
             let tab = str.split(/\n/)
-            tab = tab[1].split(tab[0])
-            for (let i = 0; i < tab.length; i++) {
-                result += parseFloat(tab[i])
-            }
-            return "" + result.toFixed(1)
+            return this.addFromATab(tab[1].split(tab[0]))
         }
         let tab = str.split(/,|\n/)
+        return this.addFromATab(tab)
+    }, 
+    addFromATab(tab){
+        result = 0 
         for (let i = 0; i < tab.length; i++) {
             if (tab[i] === ''){
                 throw new Error(`Bad structure, 2 separators on the way`)
