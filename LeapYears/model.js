9eaf8f7de02811c200cc6146200dba9460a72a18
Romain <FriiZe@MacBook-Pro-de-FriiZe.local>
2020-03-06 12:08:48 +0100

    refacto

--- LeapYears/model.js
+++ LeapYears/model.js
@@ -1,5 +1,6 @@
 module.exports = {
     leapyear(num) {
-        return (Number.isInteger(num/400))   
+        return (Number.isInteger(num/400) || !Number.isInteger(num/100))//|| (Number.isInteger(num/4) && !Number.isInteger(num/100)))
+
     }
 }
