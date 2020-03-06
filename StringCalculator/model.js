1a4bdfee8d00ee654db2688216a44156f4bf6f0b
Romain <FriiZe@MacBook-Pro-de-FriiZe.local>
2020-03-06 16:02:00 +0100

    green phase custom separator

--- StringCalculator/model.js
+++ StringCalculator/model.js
@@ -2,6 +2,15 @@ module.exports = {
     add(str) {
         if (str === "") return "0"
         let result = 0 
+        if (str.startsWith("//")){
+            str = str.substring(2)
+            let tab = str.split(/\n/)
+            tab = tab[1].split(tab[0])
+            for (let i = 0; i < tab.length; i++) {
+                result += parseFloat(tab[i])
+            }
+            return "" + result.toFixed(1)
+        }
         let tab = str.split(/,|\n/)
         for (let i = 0; i < tab.length; i++) {
             if (tab[i] === ''){
