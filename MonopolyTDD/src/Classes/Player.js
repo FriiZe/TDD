d3ec53242f3e7a5df3af6df9fd085be6255be3de
Romain Vergé <romain@wikodit.fr>
2020-03-12 15:15:52 +0100

    [RED/GREEN] player can pay a rent to another player

--- MonopolyTDD/src/Classes/Player.js
+++ MonopolyTDD/src/Classes/Player.js
@@ -25,6 +25,50 @@ class Player {
     rollADice() {
         return Math.floor(Math.random() * Math.floor(6)) + 1
     }
+
+    payARent(property, owner) {
+        let rent = null;
+
+        if (property instanceof Gare) {
+            let count = 0
+            for (let p of owner.propertiesList) {
+                if (p instanceof Gare) {
+                    count += 1
+                }
+            }
+            rent = property.basicRent * count
+        } else {
+
+            switch (property.numberHouse) {
+                case 0:
+                    rent = property.basicRent
+                    break;
+                case 1:
+                    rent = property.oneHouseRent
+                    break;
+                case 2:
+                    rent = property.twoHouseRent
+                    break;
+                case 3:
+                    rent = property.threeHouseRent
+                    break;
+                case 4:
+                    rent = property.fourHouseRent
+                    break;
+                case 5:
+                    rent = property.hostelRent
+                    break;
+                default:
+                    break;
+            }
+        }
+        console.log("Vous payer le loyer de " + property.name + " à " + owner.name + ". Il s'élève à " + rent)
+
+        this.money -= rent
+        owner.money += rent
+        console.log("Il vous reste " + this.money)
+    }
+
 }
 
 module.exports = { Player };
--- MonopolyTDD/tests/Classes/Player.js
+++ MonopolyTDD/tests/Classes/Player.js
@@ -1,5 +1,7 @@
 const chai = require('chai'); // Using Expect style
 const { Player } = require('../../src/Classes/Player');
+const { Properties } = require("../../src/Classes/Properties");
+const { Gare } = require("../../src/Classes/Gare");
 
 
 const { assert, expect } = chai;
@@ -8,6 +10,15 @@ chai.use(require('chai-like'));
 chai.use(require('chai-things'));
 
 const player = new Player('Toto')
+const player2 = new Player('Tata')
+
+const classicProperty = new Properties("VueJS", "brown", 80, 4, 20, 60, 180, 320, 450, 40, 50)
+const gare1 = new Gare("YnovCampusNantes")
+const gare2 = new Gare("YnovCampusBordeaux")
+const gare3 = new Gare("YnovCampusLyon")
+const gare4 = new Gare("YnovCampusParis")
+
+
 
 describe('Test player', () => {
     it('Test rolldice', () => {
@@ -19,5 +30,36 @@ describe('Test player', () => {
            if (diceRoll === undefined || diceRoll < 1 || diceRoll > 6) throw new Error('Le lancer doit retourner un nombre entre 1 et 6')
        }
     })
-    
+    describe("Pay a rent", () => {
+        it('Pay a classic rent', () => {   
+            player2.propertiesList.push(classicProperty)
+            player.payARent(classicProperty, player2)
+            assert.equal(player.money, 1496)
+            assert.equal(player2.money, 1504)
+        })
+        it('Pay a rent with one train station', () => {
+            player2.propertiesList.push(gare1)
+            player.payARent(gare1, player2)
+            assert.equal(player.money, 1446)
+            assert.equal(player2.money, 1554)
+        })
+        it('Pay a rent with two train stations', () => {
+            player2.propertiesList.push(gare2)
+            player.payARent(gare2, player2)
+            assert.equal(player.money, 1346)
+            assert.equal(player2.money, 1654)
+        })
+        it('Pay a rent with three train stations', () => {
+            player2.propertiesList.push(gare3)
+            player.payARent(gare3, player2)
+            assert.equal(player.money, 1196)
+            assert.equal(player2.money, 1804)
+        })
+        it('Pay a rent with four train stations', () => {
+            player2.propertiesList.push(gare4)
+            player.payARent(gare4, player2)
+            assert.equal(player.money, 996)
+            assert.equal(player2.money, 2004)
+        })
+    }) 
 })
